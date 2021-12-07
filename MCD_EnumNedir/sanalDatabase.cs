f8d16663743946d404da4de55ddd2648899adced
emre gemici <gmcc.emree@gmail.com>
2021-12-07 16:20:11 +0300

    Add project files.

---
+++ MCD_EnumNedir/sanalDatabase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+
+namespace MCD_EnumNedir
+{
+    public class sanalDatabase
+    {
+        ArrayList sanalDB = new ArrayList();
+
+    }
+}
